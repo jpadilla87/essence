d99d801baeac3e5e963df2901a55903f9521c48e
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-10-27 18:48:48 -0500

    chore: swapped line for shop and cart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Navbar} from "./components/navbar";
 function App() {
   return (
     <div className="App">
       <Router>
-
+        <Navbar />
         <Routes>
           <Route path="/" />
-          <Route path="/cart" />
           <Route path="/shop" />
-
+          <Route path="/cart" />
         </Routes>
       </Router>
     </div>
