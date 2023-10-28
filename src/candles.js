a1fb0fb57606b765e487b17313a49a3cbca025a5
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-10-28 11:35:49 -0500

    feat: add candles and elements

---
+++ src/candles.js
@@ -0,0 +1,94 @@
+import candle1 from "./assets/1AutumnSpice.png";
+import candle2 from "./assets/2CitrusBurst.png";
+import candle3 from "./assets/3CozyFireside.png";
+import candle4 from "./assets/4FreshLinen.png";
+import candle5 from "./assets/5LavenderDreams.png";
+import candle6 from "./assets/6MidnightRose.png";
+import candle7 from "./assets/7MountainPine.png";
+import candle8 from "./assets/8OceanBreeze.png";
+import candle9 from "./assets/9TropicalParadise.png";
+import candle10 from "./assets/10VanillaDelight.png";
+
+export const CANDLES = [
+    {
+        candleID: 1,
+        candleName: "Autumn Spice",
+        price: 15.99,
+        candleImage: candle1,
+        category: "Sweet",
+    },
+    {
+        candleID: 2,
+        candleName: "Citrus Burst",
+        price: 12.99,
+        candleImage: candle2,
+        category: "Fruity",
+    },
+    {
+        candleID: 3,
+        candleName: "Cozy Fireside",
+        price: 16.99,
+        candleImage: candle3,
+        category: "Fresh",
+    },
+    {
+        candleID: 4,
+        candleName: "Fresh Linen",
+        price: 11.99,
+        candleImage: candle4,
+        category: "Fresh",
+    },
+    {
+        candleID: 5,
+        candleName: "Lavender Dreams",
+        price: 16.99,
+        candleImage: candle5,
+        category: "Floral",
+    },
+    {
+        candleID: 6,
+        candleName: "Midnight Rose",
+        price: 13.99,
+        candleImage: candle6,
+        category: "Floral",
+    },
+    {
+        candleID: 7,
+        candleName: "Mountain Pine",
+        price: 11.99,
+        candleImage: candle7,
+        category: "Fresh",
+    },
+    {
+        candleID: 8,
+        candleName: "Ocean Breeze",
+        price: 13.99,
+        candleImage: candle8,
+        category: "Fresh",
+    },
+    {
+        candleID: 9,
+        candleName: "Tropical Paradise",
+        price: 12.99,
+        candleImage: candle9,
+        category: "Fruity",
+    },
+    {
+        candleID: 10,
+        candleName: "Vanilla Delight",
+        price: 10.99,
+        candleImage: candle10,
+        category: "Sweet",
+    }
+];
+
+// Autumn Spice - Category: Sweet ORANGE
+// Citrus Burst - Category: Fruity YELLOW/ORANGE
+// Cozy Fireside - Category: Fresh SLATE
+// Fresh Linen - Category: Fresh LIGHT BLUE
+// Lavender Dreams - Category: Floral PURPLE
+// Midnight Rose - Category: Floral RED
+// Mountain Pine - Category: Fresh GREEN
+// Ocean Breeze - Category: Fresh BLUE
+// Tropical Paradise - Category: Fruity BLUE/GREEN
+// Vanilla Delight - Category: Sweet WHITE
