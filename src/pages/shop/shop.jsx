3d9f0c0ae922005f99aed181fecf968ad1bd082e
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-10-27 23:39:33 -0500

    feat: connected routes for navbar links

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+        <div>
+            <h1>Essence</h1>
+            <h2>Luxury Fragrances</h2>
+        </div>
+        <div className="candles">
+            
+        </div>
+    </div>
+  )
+}
