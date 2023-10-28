744eab20c0954cc0ca839fee2056c6e72ba0132e
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-10-28 14:06:05 -0500

    feat: show number of items added after button

--- src/pages/shop/candle.jsx
+++ src/pages/shop/candle.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shop-context';
 
 export const Candle = (props) => {
     const {candleID, candleName, price, candleImage, category} = props.data;
+    const { addToCart, cartItems } = useContext(ShopContext);
+
+    const cartItemAmount = cartItems[candleID]
   return <div className="candle">
     <img src={candleImage} alt="Candle SHot" />
     <div className="description">
@@ -12,5 +16,8 @@ export const Candle = (props) => {
             ${price}
         </p>
     </div>
+    <button className="addToCartBttn" onClick={() => addToCart(candleID)}>
+        Add to Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+    </button>
 </div>;
 }
