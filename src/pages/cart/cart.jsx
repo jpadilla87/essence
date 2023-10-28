05480a31fae7f303732a3a699426441cdb51705c
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-10-28 16:48:41 -0500

    feat: add cart page with functions

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { CANDLES } from "../../candles";
+import { ShopContext } from "../../context/shop-context";
+import { CartItem } from "./cart-item";
+import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
   return (
-    <div>cart</div>
-  )
-}
+    <div className="cart">
+      <div>
+        <h1>Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {CANDLES.map((candle) => {
+          if (cartItems[candle.candleID] !== 0) {
+            return <CartItem data={candle} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>
+            Subtotal: <b>${totalAmount.toFixed(2)}</b>
+          </p>
+          <button onClick={() => navigate("/shop")}> Continue Shopping </button>
+          <button> Checkout </button>
+        </div>
+      ) : (
+        <h2> Your Cart is Empty. </h2>
+      )}
+    </div>
+  );
+};
--- src/pages/shop/candle.jsx
+++ src/pages/shop/candle.jsx
@@ -1,23 +1,23 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../../context/shop-context';
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export const Candle = (props) => {
-    const {candleID, candleName, price, candleImage, category} = props.data;
-    const { addToCart, cartItems } = useContext(ShopContext);
+  const { candleID, candleName, price, candleImage, category } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
 
-    const cartItemAmount = cartItems[candleID]
-  return <div className="candle">
-    <img src={candleImage} alt="Candle SHot" />
-    <div className="description">
+  const cartItemAmount = cartItems[candleID];
+  return (
+    <div className="candle">
+      <img src={candleImage} alt="Candle Shot" />
+      <div className="description">
         <p>
-            <b>{candleName}</b>
+          <b>{candleName}</b>
         </p>
-        <p>
-            ${price}
-        </p>
-    </div>
-    <button className="addToCartBttn" onClick={() => addToCart(candleID)}>
+        <p>${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(candleID)}>
         Add to Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
-    </button>
-</div>;
-}
+      </button>
+    </div>
+  );
+};
