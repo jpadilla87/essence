a06bc3f39c807e4bd81f2540ad9c527fb0f6e0c8
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-11-11 01:23:24 -0600

    chore: add port environment

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,43 @@
-require('dotenv').config();
-const http = require('http');
+require("dotenv").config();
+const http = require("http");
 
-const { sendResponse } = require('./helper/response');
-const candleRoutes = require('./routes/candles');
+const { sendResponse } = require("./helper/response");
+const candleRoutes = require("./routes/candles");
 
 const getHandlers = {
-        '/candles/all': (req, res) => candleRoutes.getCandles(req, res),
-        '/candles': (req, res) => candleRoutes.getCandleByID(req, res),
-        '/candles/price': (req, res) => candleRoutes.getCandlePriceByID(req, res),
-}
+  "/candles/all": (req, res) => candleRoutes.getCandles(req, res),
+  "/candles": (req, res) => candleRoutes.getCandleByID(req, res),
+  "/candles/price": (req, res) => candleRoutes.getCandlePriceByID(req, res),
+};
 
-const postHandlers = {
-
-}
+const postHandlers = {};
 
 const server = http.createServer(async (req, res) => {
-    let searchURL = req.url.split('?')[0];
-    const getHandler = getHandlers[searchURL];
-    const postHandler = postHandlers[searchURL];
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    if (req.method === 'OPTIONS') {
-        // Handle preflight request
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST', 'PUT', 'DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.writeHead(204);
-        res.end();
-        return;
-    }
+  let searchURL = req.url.split("?")[0];
+  const getHandler = getHandlers[searchURL];
+  const postHandler = postHandlers[searchURL];
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    // Handle preflight request
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 
-    if (getHandler && req.method === 'GET') {
-            getHandler(req, res);
-    } else if (postHandler && req.method === 'POST') {
-            postHandler(req, res);
-    } else {
-            sendResponse(req, res, 404, 'Not found');
-    }
+  if (getHandler && req.method === "GET") {
+    getHandler(req, res);
+  } else if (postHandler && req.method === "POST") {
+    postHandler(req, res);
+  } else {
+    sendResponse(req, res, 404, "Not found");
+  }
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log("Server running on port:", PORT));
