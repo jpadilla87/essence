866cf18c3b70f764fde2d622e2209dd82a860fe0
Jorell Padilla <92110724+jpadilla87@users.noreply.github.com>
2023-11-20 22:41:35 -0600

    feat: add dashboard page for admin

--- app/app/dashboard/page.jsx
+++ app/app/dashboard/page.jsx
@@ -1,31 +1,44 @@
+"use client";
 import React from 'react';
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
+
+  const router = useRouter();
+  
   const buttonStyle = {
-    backgroundColor: '#4CAF50', /* Green background color */
-    border: 'none', /* Remove borders */
-    color: 'white', /* White text */
-    padding: '15px 32px', /* Some padding */
-    textAlign: 'center', /* Center text */
+    backgroundColor: '#4CAF50',
+    border: 'none',
+    color: 'white',
+    padding: '15px 32px',
+    textAlign: 'center',
     textDecoration: 'none',
     display: 'inline-block',
-    fontSize: '16px', /* Set font size */
-    margin: '4px 2px', /* Add some margin */
-    cursor: 'pointer', /* Add a pointer cursor on hover */
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
   };
 
   const containerStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    height: '100vh', // 100% of the viewport height
+    height: '100vh',
+  };
+
+  const navigateToProducts = () => {
+    // Use the router to navigate to the Products page
+    router.push("/dashboard/products");
   };
 
   return (
     <div style={containerStyle}>
       <h1>Dashboard</h1>
       <div>
-        <button style={buttonStyle}>Products</button>
+        {/* Add an onClick event to trigger navigation */}
+        <button style={buttonStyle} onClick={navigateToProducts}>
+          Products
+        </button>
         <button style={buttonStyle}>Transactions</button>
         <button style={buttonStyle}>Queries</button>
         <button style={buttonStyle}>Reports</button>
